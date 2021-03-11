1cd8366e600caccddd5bcb8b5db767db2bf59829
nurgnc <gencbedriyenur@gmail.com>
2021-03-11 18:43:42 +0300

    created banner slider.

---
+++ 02-OnlineStoreE-commerce/js/main.js
@@ -0,0 +1,10 @@
+// First Slider
+$(".slider-one")
+    .not(".slick-intialized")
+    .slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        prevArrow: ".site-slider .slider-btn .prev",
+        nextArrow: ".site-slider .slider-btn .next",
+    });
