b1042f36039ca7deb4627d9ebdd434523f76d5cd
nurgnc <gencbedriyenur@gmail.com>
2021-03-15 18:27:44 +0300

    initial commit

--- 02-OnlineStoreE-commerce/js/main.js
+++ 02-OnlineStoreE-commerce/js/main.js
@@ -3,8 +3,19 @@ $(".slider-one")
     .not(".slick-intialized")
     .slick({
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 3000,
         dots: true,
         prevArrow: ".site-slider .slider-btn .prev",
         nextArrow: ".site-slider .slider-btn .next",
+    });
+// Second Slider
+$(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: ".site-slider-two .prev",
+        nextArrow: ".site-slider-two .next",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
     });
